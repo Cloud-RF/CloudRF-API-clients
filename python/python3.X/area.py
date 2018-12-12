a5e8bb663fe324242a8765d167796446faef50db
Ernest Baker <baker5792@gmail.com>
2018-12-12 08:39:51 -0500

    Initial maintainablility changes.

---
+++ python/python3.X/area.py
@@ -0,0 +1,43 @@
+import requests, csv, sys, os, time, json
+server="https://cloudrf.com" 
+
+if len(sys.argv) == 1:
+	print "ERROR: Need a .csv file\neg. python coverage.py mydata.csv"
+	quit()
+	
+if not os.path.exists("calculations"):
+		os.makedirs("calculations")
+
+# Open CSV file
+csvfile = csv.DictReader(open(sys.argv[1]))
+n=0
+for row in csvfile:
+	# Pause script. Important otherwise server will ban you.
+	time.sleep(1)
+	start_time = time.time() # Stopwatch start
+	#print row
+	r = requests.post(server+"/API/area", data=row)
+	print r.text
+	#try:
+	j = json.loads(r.text)
+	if 'kmz' in j:
+		#print j['kmz']
+		r = requests.get(j['kmz'])
+		fn="calculations"+os.sep+str(row['nam'])+".kmz"
+		file = open(fn,"wb")
+		file.write(r.content)
+		file.close()
+		print "Saved to %s" % fn
+	if 'shp' in j:
+		#print j['kmz']
+		r = requests.get(j['shp'])
+		fn="calculations"+os.sep+str(row['nam'])+".shp.zip"
+		file = open(fn,"wb")
+		file.write(r.content)
+		file.close()
+		print "Saved to %s" % fn
+
+
+	elapsed = round(time.time() - start_time,1) # Stopwatch
+	print "Elapsed: "+str(elapsed)+"s"
+	n=n+1
