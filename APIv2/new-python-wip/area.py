53499dee7ffd8b0f29b0e93d23baeb848e2fdfb5
Alex Winder <contact@alexwinder.uk>
2023-01-31 16:35:16 +0000

    Python 3.8 minimum tested

--- APIv2/new-python-wip/area.py
+++ APIv2/new-python-wip/area.py
@@ -50,7 +50,7 @@ def checkValidJsonTemplate():
 class PythonValidator:
     def version():
         requiredMajor = 3
-        minimumMinor = 9
+        minimumMinor = 8
 
         if sys.version_info.major != requiredMajor or sys.version_info.minor < minimumMinor:
             sys.exit('Your Python version (%s) does not meet the minimum required version of %s' % (
@@ -100,3 +100,4 @@ if __name__ == '__main__':
 
     checkPermissions()
     jsonTemplate = checkValidJsonTemplate()
+
