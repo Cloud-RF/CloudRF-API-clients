f75c37a53c420347fdeff056445de5686c6aaa1b
Alex Winder <contact@alexwinder.uk>
2023-02-07 15:54:13 +0000

    Validate the API key on load

--- APIv2/slippy-maps/CloudRF.js
+++ APIv2/slippy-maps/CloudRF.js
@@ -13,7 +13,10 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 var apiKey = undefined;
 var apiServiceBaseUrl = undefined;
 
-setApiServiceBaseUrl();
+$(document).ready(function() {
+    setApiServiceBaseUrl();
+    validateApiKey();
+})
 
 $('input#apiKey').on('change', function() {
     apiKey = $('input#apiKey').val();
